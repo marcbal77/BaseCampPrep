eb9c09ef83e37523341cc08474a32361fbe2e6c3
Marc Balestreri <hello@marcbal.com>
2024-04-19 16:35:18 -0700

    Creating initial function called fizzBuzz for Control Structures Exercise

--- contracts/ControlStructures.sol
+++ contracts/ControlStructures.sol
@@ -5,3 +5,12 @@ pragma solidity 0.8.19;
 import "hardhat/console.sol";
 
 contract ControlStructures {
+
+    function fizzBuzz(uint _number) external pure returns(string memory) {
+        // return "Fizz" if the _number is divisible by 3
+        // return "Buzz" if the _number is divisible by 5
+        // return "FizzBuzz" if the _number is divisible by 3 and 5
+        // return "Splat" if none of the above conditions are true
+    }
+
+}
