3be1f659e23448971a61006b059c6292a653d1f2
Marc Balestreri <hello@marcbal.com>
2024-04-19 17:13:55 -0700

    First part of FizzBuzz Exercise Function

--- contracts/ControlStructures.sol
+++ contracts/ControlStructures.sol
@@ -11,6 +11,18 @@ contract ControlStructures {
         // return "Buzz" if the _number is divisible by 5
         // return "FizzBuzz" if the _number is divisible by 3 and 5
         // return "Splat" if none of the above conditions are true
+        if (_number % 3 == 0 && _number % 5 == 0) {
+            // return "FizzBuzz" if the _number is divisible by 3 and 5
+            return "FizzBuzz";
+        } else if (_number % 3 == 0) {
+            // The number is divisible by 3
+            return "Fizz";
+        } else if (_number % 5 == 0) {
+            // return "Buzz" if the _number is divisible by 5            
+            return "Buzz";    
+        } else {
+            // return "Splat" if none of the above conditions are true
+            return "Splat";                    
+        }
     }
-
 }
